cb69349a841a655942e031feae575494df1ed628
Roswer García <roswer13@gmail.com>
2026-01-11 09:52:20 +0100

    Add TeamInfo component and update PredictionCard to display team logos and match status

--- lib/features/prediction/presentation/room/components/prediction_card.dart
+++ lib/features/prediction/presentation/room/components/prediction_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:porrapp_frontend/features/prediction/domain/models/models.dart';
 import 'package:porrapp_frontend/features/prediction/presentation/room/components/score_input.dart';
+import 'package:porrapp_frontend/features/prediction/presentation/room/components/team_info.dart';
 
 class PredictionCard extends StatefulWidget {
   final PredictionModel prediction;
@@ -22,29 +23,45 @@ class _PredictionCardState extends State<PredictionCard> {
         padding: const EdgeInsets.all(12),
         child: Column(
           children: [
-            Text('Match ${match.id}'),
-            Text('${match.homeScore} - ${match.awayScore}'),
+            Text('Match ${match.id} - ${match.date.toLocal()}'),
+            match.isFinished
+                ? const Text(
+                    'Match Finished',
+                    style: TextStyle(color: Colors.red),
+                  )
+                : const SizedBox.shrink(),
 
-            const SizedBox(height: 8),
+            Text('${match.homeScore} - ${match.awayScore}'),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                TeamInfo(
+                  teamName: match.homeTeam.name,
+                  teamLogoUrl: match.homeTeam.flag,
+                ),
+                const SizedBox(width: 21),
                 const ScoreInput(),
                 const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  padding: EdgeInsets.symmetric(horizontal: 16),
                   child: Text(':'),
                 ),
                 const ScoreInput(),
+
+                const SizedBox(width: 21),
+                TeamInfo(
+                  teamName: match.awayTeam.name,
+                  teamLogoUrl: match.awayTeam.flag,
+                ),
               ],
             ),
 
-            const SizedBox(height: 8),
+            // const SizedBox(height: 8),
 
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Guardar predicción'),
-            ),
+            // ElevatedButton(
+            //   onPressed: () {},
+            //   child: const Text('Guardar predicción'),
+            // ),
           ],
         ),
       ),
