586124a1306fe56321cbc8a6f3365127ebaea0d0
Roswer García <roswer13@gmail.com>
2026-01-16 14:29:49 +0100

    Add internationalization support with English and Spanish translations

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:porrapp_frontend/core/bloc_providers.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_logs/flutter_logs.dart';
 
+import 'package:porrapp_frontend/core/bloc_providers.dart';
 import 'package:porrapp_frontend/core/design/theme.dart';
 import 'package:porrapp_frontend/core/env/env.dart';
 import 'package:porrapp_frontend/core/injection.dart';
 import 'package:porrapp_frontend/core/router/router.dart';
+import 'package:porrapp_frontend/l10n/app_localizations.dart';
 
 Future<void> bootstrap({required String env}) async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,6 +31,29 @@ class PorraApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: blocProviders(),
       child: MaterialApp.router(
+        supportedLocales: const [
+          Locale('en'), // English
+          Locale('es'), // Spanish
+        ],
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        localeResolutionCallback: (locale, supportedLocales) {
+          FlutterLogs.logInfo(
+            'PorraApp',
+            'localeResolutionCallback',
+            'Device locale: ${locale?.languageCode}, Supported locales: $supportedLocales',
+          );
+          for (var supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale?.languageCode) {
+              return supportedLocale;
+            }
+          }
+          return supportedLocales.first;
+        },
         routerConfig: appRouter,
         title: envConfig.appName,
         theme: MaterialTheme(TextTheme()).light(),
