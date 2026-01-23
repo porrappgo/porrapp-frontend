8d867143acd08ed77763183561c5a10da0a69a20
Roswer García <roswer13@gmail.com>
2026-01-23 09:17:38 +0100

    Refactor test coverage script: streamline directory creation and improve output messages

--- scripts/test-with-coverage.sh
+++ scripts/test-with-coverage.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-## Project paths (no Melos)
 PROJECT_ROOT_PATH="$(pwd)"
 PACKAGE_PATH="."
 PACKAGE_NAME="$(basename "$PROJECT_ROOT_PATH")"
@@ -9,23 +8,27 @@ PACKAGE_NAME="$(basename "$PROJECT_ROOT_PATH")"
 PACKAGE_LCOV_INFO_PATH="$PROJECT_ROOT_PATH/coverage/lcov_$PACKAGE_NAME.info"
 PACKAGE_TEST_REPORT_PATH="$PROJECT_ROOT_PATH/test_reports/${PACKAGE_NAME}_test_report.json"
 
-mkdir -p "$PROJECT_ROOT_PATH/coverage"
-mkdir -p "$PROJECT_ROOT_PATH/test_reports"
+mkdir -p "$PROJECT_ROOT_PATH/coverage" "$PROJECT_ROOT_PATH/test_reports"
+
+echo "▶ Running flutter test..."
 
 flutter test \
-  --no-pub \
   --machine \
   --coverage \
   --coverage-path "$PACKAGE_LCOV_INFO_PATH" \
   > "$PACKAGE_TEST_REPORT_PATH"
 
+if [[ ! -f "$PACKAGE_LCOV_INFO_PATH" ]]; then
+  echo "❌ Coverage file not generated"
+  exit 1
+fi
+
 escapedPath="$(echo "$PACKAGE_PATH" | sed 's/\//\\\//g')"
 
-# macOS needs gsed
 if [[ "$OSTYPE" =~ ^darwin ]]; then
   gsed -i "s/^SF:lib/SF:$escapedPath\/lib/g" "$PACKAGE_LCOV_INFO_PATH"
 else
   sed -i "s/^SF:lib/SF:$escapedPath\/lib/g" "$PACKAGE_LCOV_INFO_PATH"
 fi
 
-echo "✅ Coverage generated."
+echo "✅ Coverage generated at $PACKAGE_LCOV_INFO_PATH"
